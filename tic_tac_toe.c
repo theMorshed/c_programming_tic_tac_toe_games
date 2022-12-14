8b6bdc11613de5b9a2e22f12dfc844740977a819
theMorshed <themorshedctg@gmail.com>
2022-12-15 03:19:09 +0600

    print winner structure function implement

--- tic_tac_toe.c
+++ tic_tac_toe.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-// print structure function prototype
+// print structure and who is win function prototype
 void print_structure(int table[][4], int n);
+void print_winner_structure(int table[][4], int n, int m);
 int who_is_win(int table[][4], int n);
 
 int main()
@@ -51,11 +52,11 @@ int main()
         // check who is win
         if (who_is_win(table, n) == 1) {
             printf("Player 1 is Won!\n");
-            print_structure(table, n);
+            print_winner_structure(table, n, 1);
             break;
         } else if (who_is_win(table, n) == 2) {
             printf("Player 2 is Won!\n");
-            print_structure(table, n);
+            print_winner_structure(table, n, 2);
             break;
         }
     }
@@ -86,24 +87,51 @@ void print_structure(int table[][4], int n)
     }
 }
 
+void print_winner_structure(int table[][4], int n, int m)
+{
+    printf("\n");
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (table[i][j] == -1)
+                printf(" ");
+            else if (table[i][j] == 1 && m == 1)
+                printf("X");
+            else if (table[i][j] == 2 && m == 2) 
+                printf("O");
+
+            if (j < n) 
+                printf("\t|\t");
+        }
+        printf("\n");
+        if (i < n) 
+            printf("__________________________________");
+        printf("\n\n");
+    }
+}
+
+// who is win function which return winner player number
 int who_is_win(int table[][4], int n)
 {
+    // check for all row match
     for (int i = 1; i <= 3; i++) {
         if (table[i][1] == table[i][2] && table[i][2] == table[i][3] && table[i][1] != -1) {
             return table[i][1];
         }
     }
 
+    // check for all column match 
     for (int j = 1; j <= 3; j++) {
         if (table[1][j] == table[2][j] && table[2][j] == table[3][j] && table[1][j] != -1) {
             return table[1][j];
         }
     }
 
+    // check diagonal match
     if (table[1][1] == table[2][2] && table[2][2] == table[3][3] && table[1][1] != -1) {
         return table[1][1];
     }
 
+    // check diagnonal match
     if (table[1][3] == table[2][2] && table[2][2] == table[3][1] && table[1][3] != -1) {
         return table[1][3];
     }
